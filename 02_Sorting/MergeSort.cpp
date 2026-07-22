04f6b1f245f6faa0d2f2f51e928698fdca1be56f
gunguncodes <gungunkri865@gmail.com>
2026-07-22 12:23:40 +0530

    Program: Merge Sort

---
+++ 02_Sorting/MergeSort.cpp
@@ -0,0 +1,77 @@
+/*
+Topic: Sorting
+Algorithm: Merge Sort
+Worst Time Complexity: 
+Best Time Complexity: 
+Space Complexity: 
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge(vector<int>& arr, int low, int mid, int high) {
+    int left = low;
+    int right = mid+1;
+    vector<int>temp;
+
+    while(left <= mid && right <= high) {
+        if(arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while(left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i=low; i<=high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+
+    if(low >= high) {
+        return;
+    }
+
+    int mid = (low+high)/2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int x : arr)
+        cout << x << " ";
+
+    return 0;
+}
